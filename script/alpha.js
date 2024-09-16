9b08abc6fe13f8c2443d23267bb9cccf8949c2e4
runshuvo <nhshuvo2003@gmail.com>
2024-09-16 21:28:59 +0600

    first

---
+++ script/alpha.js
@@ -0,0 +1,6 @@
+function play(){
+    const home = document.getElementById('home');
+    home.classList.add('hidden');
+    const play =document.getElementById('play');
+    play.classList.remove('hidden');
+}
