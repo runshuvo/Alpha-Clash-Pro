116ca65e140668d67ba27a218730bb137c057046
runshuvo <nhshuvo2003@gmail.com>
2024-09-17 00:26:40 +0600

    first

--- script/alpha.js
+++ script/alpha.js
@@ -3,4 +3,28 @@ function play(){
     home.classList.add('hidden');
     const play =document.getElementById('play');
     play.classList.remove('hidden');
+    randomKey();
+    addRandomText();
+}
+function randomKey(){
+    const randomNum=Math.random() * 25 ;
+    const index = Math.round(randomNum);
+    console.log(index);
+
+    const alphabatestring = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabates =alphabatestring.split('') ;
+    
+    const alphabt =alphabates[index];
+    console.log(alphabt);
+    
+
+    const textArea = document.getElementById('textArea');
+    textArea.innerText = alphabt ;
+
+   const btn = document.getElementById(alphabt);
+   btn.classList.add('bg-orange-400');
+}
+function highlightingText(){
+
+   
 }
