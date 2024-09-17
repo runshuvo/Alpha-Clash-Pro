bbb64b2fe41044ac1a9bfb3638ce5bce93aa5d67
runshuvo <nhshuvo2003@gmail.com>
2024-09-17 14:03:05 +0600

    third

--- script/alpha.js
+++ script/alpha.js
@@ -1,3 +1,17 @@
+function playAgain(){
+    
+    const playCard=document.getElementById('play');
+    playCard.classList.remove('hidden');
+
+    const scoreCard=document.getElementById('scoreCard');
+    scoreCard.classList.add('hidden');
+    
+    addElementValueById('score',0);
+    addElementValueById('life',5);
+
+
+}
+
 function play(){
     const home = document.getElementById('home');
     home.classList.add('hidden');
@@ -5,6 +19,70 @@ function play(){
     play.classList.remove('hidden');
     randomKey();
     addRandomText();
+}
+
+function removeBg(ElementId){
+    const element =document.getElementById(ElementId);
+    element.classList.remove('bg-orange-400');
+
+}
+function addElementValueById(ElementId,value){
+    const id=document.getElementById(ElementId);
+    id.innerText=value;
+}
+
+
+document.addEventListener('keyup',keyPressHandle )
+function keyPressHandle(event){
+const keyPressed = event.key ;
+console.log(keyPressed);
+const keyShowed =document.getElementById('textArea');
+const keyshowed =keyShowed.innerText.toLowerCase();
+
+if(keyPressed===keyshowed){
+    console.log('You got point');
+    randomKey();
+   removeBg(keyshowed);
+   const currentScore =document.getElementById('score');
+    const currentScoreText=currentScore.innerText;
+    const currentScoreNum=parseInt(currentScoreText);
+
+const newScore =currentScoreNum + 1;
+currentScore.innerText =newScore;
+
+
+}
+else{
+    console.log('you lost a point');
+const currentLife=document.getElementById('life');
+const currentLifeText =currentLife.innerText;
+const currentLifeNum =parseInt(currentLifeText);
+
+
+const newLife=currentLifeNum - 1;
+currentLife.innerText = newLife ;
+
+if(currentLifeNum <= 0){
+console.log(
+    'gameover'
+)
+    const playCard=document.getElementById('play');
+    playCard.classList.add('hidden');
+
+    const scoreCard=document.getElementById('scoreCard');
+    scoreCard.classList.remove('hidden');
+
+
+    const scoreNum=document.getElementById('score');
+    const text =scoreNum.innerText
+    addElementValueById('scoreNum',text);
+
+}
+
+
+}
+
+
 }
 function randomKey(){
     const randomNum=Math.random() * 25 ;
@@ -23,8 +101,4 @@ function randomKey(){
 
    const btn = document.getElementById(alphabt);
    btn.classList.add('bg-orange-400');
-}
-function highlightingText(){
-
-   
 }
